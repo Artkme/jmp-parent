f524962eb60cc7969779e878f41ec6220c9983ec
Saba Artkmeladze <saba.artkmeladze@gmail.com>
2025-07-19 19:56:04 +0400

    Implement methods in ServiceImpl

--- jmp-cloud-service-impl/src/main/java/com/epam/jmp/impl/ServiceImpl.java
+++ jmp-cloud-service-impl/src/main/java/com/epam/jmp/impl/ServiceImpl.java
@@ -5,24 +5,32 @@ import com.epam.jmp.dto.Subscription;
 import com.epam.jmp.dto.User;
 import com.epam.jmp.service.Service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.*;
 
 
 public class ServiceImpl implements Service {
+
+    private final Map<String, Subscription> subscriptions = new HashMap<>();
+    private final Set<User> users = new HashSet<>();
+
     @Override
     public void subscribe(BankCard bankCard) {
-
+        subscriptions.computeIfAbsent(
+                bankCard.getNumber(),
+                num -> new Subscription(num, LocalDate.now())
+        );
+        users.add(bankCard.getUser());
     }
 
     @Override
     public Optional<Subscription> getSubscriptionByBankCardNumber(String cardNumber) {
-        return Optional.empty();
+        return Optional.ofNullable(subscriptions.get(cardNumber));
     }
+
 
     @Override
     public List<User> getAllUsers() {
-        return Collections.EMPTY_LIST;
+        return users.stream().toList();
     }
 }
